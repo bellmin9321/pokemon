0cf2a6a112087143b28c888117371c0559e89b9a
JONGMIN KIM <bellmin9321@gmail.com>
2023-06-13 22:09:18 +0900

    ✨ Feat: react-query, recoil provider 및 라우팅 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from './lib/api/queryClient';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/layout';
+import Home from './components/pages/Home';
+import { RecoilRoot } from 'recoil';
+import Loading from './components/Loading';
+import PokemonDetails from './components/pages/PokemonDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Suspense fallback={<Loading />}>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path={`/pokemon/:name`} element={<PokemonDetails />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
