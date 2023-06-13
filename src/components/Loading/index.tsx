d62b0a31de98b7cdf778e4c019fc198e3fda6cee
JONGMIN KIM <bellmin9321@gmail.com>
2023-06-13 22:12:25 +0900

    ✅ Add: Loading 컴포넌트 추가

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,23 @@
+import { memo } from 'react';
+import ReactLoading from 'react-loading';
+import styled from 'styled-components';
+
+const Loader = () => {
+  return (
+    <LoadingWrapper>
+      <ReactLoading type="spin" color="#406bc9" />
+    </LoadingWrapper>
+  );
+};
+
+const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  padding: 10px 0 10px 0;
+`;
+
+export default memo(Loader);
