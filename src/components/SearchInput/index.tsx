e23f6883bf8501db7800f30ba6b83c736ed205de
JONGMIN KIM <bellmin9321@gmail.com>
2023-06-16 03:16:38 +0900

    🌳 Refactor: useInput 커스텀훅 생성 및 리팩토링

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,35 +1,15 @@
 import styled from 'styled-components';
-import { ChangeEvent } from 'react';
 import { INPUT_PLACEHOLDER } from '../../constants';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { inputSearchValue } from '../../lib/recoil';
+import useInput from '../../lib/hooks/useInput';
 
 function SearchInput() {
-  const [searchValue, setSearchValue] =
-    useRecoilState<string>(inputSearchValue);
-  const navigate = useNavigate();
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(e.target.value);
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      if (0 < parseInt(searchValue) && parseInt(searchValue) <= 1010) {
-        setSearchValue('');
-        navigate(`/pokemon/${searchValue}`);
-      } else {
-        alert('1~1010번까지 번호만 검색 가능합니다\nex: 1');
-      }
-    }
-  };
+  const { searchValue, handleChange, handleKeyDown } = useInput();
 
   return (
     <InputContainer>
-      <h2>좋아하는 포켓몬 번호를 검색해보세요</h2>
       <Input
         type="search"
+        value={searchValue}
         placeholder={INPUT_PLACEHOLDER}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
@@ -43,12 +23,14 @@ const InputContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding-bottom: 20px;
+  position: absolute;
+  left: 0;
+  right: 0;
 `;
 
 const Input = styled.input`
   display: block;
-  width: 40vw;
+  width: 30vw;
   height: 32px;
   line-height: 1.42;
   padding: 6px 12px;
---
+++ src/lib/hooks/useInput.ts
@@ -0,0 +1,29 @@
+import React, { ChangeEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { inputSearchValue } from '../recoil';
+
+function useInput() {
+  const navigate = useNavigate();
+  const [searchValue, setSearchValue] =
+    useRecoilState<string>(inputSearchValue);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      if (0 < parseInt(searchValue) && parseInt(searchValue) <= 1010) {
+        setSearchValue('');
+        navigate(`/pokemon/${searchValue}`);
+      } else {
+        alert('1~1010번까지 번호만 검색 가능합니다\nex: 1');
+      }
+    }
+  };
+
+  return { searchValue, handleChange, handleKeyDown };
+}
+
+export default useInput;
