cef29f88d0033a84337d1b53a4d018232fd5d5cc
JONGMIN KIM <bellmin9321@gmail.com>
2023-06-15 21:28:13 +0900

    ✨ Feat: 진화단계 표시를 위한 커스텀훅 구현

---
+++ src/lib/hooks/useEvolutionChain.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { EvolutionInfo } from '../../types';
+
+type EvoType = {
+  name: string;
+  url: string;
+} | null;
+
+function useEvolutionChain(info?: EvolutionInfo, prevEvo?: EvoType) {
+  const [evoChain, setEvoChain] = useState<EvoType[]>([]);
+
+  let evoData = info;
+
+  useEffect(() => {
+    const EvoTypes = [];
+    if (prevEvo && prevEvo.name !== evoData?.evolves_to[0].species?.name)
+      EvoTypes.push(prevEvo);
+
+    while (evoData?.evolves_to.length) {
+      if (!evoData) return;
+      const evoDetails = evoData['evolves_to'][0];
+
+      EvoTypes.push({
+        name: evoDetails?.species?.name,
+        url: evoDetails?.species?.url,
+      });
+
+      evoData = evoDetails;
+    }
+
+    setEvoChain(EvoTypes);
+  }, [info, prevEvo]);
+
+  return { evoChain };
+}
+
+export default useEvolutionChain;
