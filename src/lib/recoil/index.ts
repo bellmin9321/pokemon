3f1e85a1363b9722d039e73b0d0afe246b40cf38
JONGMIN KIM <bellmin9321@gmail.com>
2023-06-15 21:18:09 +0900

    ✅ Add: searchValue 상태 추가

---
+++ src/lib/recoil/index.ts
@@ -0,0 +1,7 @@
+import { atom, RecoilEnv } from 'recoil';
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
+
+export const inputSearchValue = atom<string>({
+  key: 'searchValue',
+  default: '',
+});
