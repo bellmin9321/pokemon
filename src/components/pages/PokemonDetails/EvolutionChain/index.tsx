7c45540aa8b22ad669ad9f238d5ffcc23bd0d1b0
JONGMIN KIM <bellmin9321@gmail.com>
2023-06-16 04:13:57 +0900

    ✅ Add: 최하위 포켓몬일 경우 자기 자신 정보 진화 단계에 추가

--- src/components/pages/PokemonDetails/EvolutionChain/index.tsx
+++ src/components/pages/PokemonDetails/EvolutionChain/index.tsx
@@ -1,54 +1,70 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Suspense, memo } from 'react';
 import {
   findPokemonDBImage,
   useGetEvolutionChain,
 } from '../../../../lib/hooks/usePokemon';
 import useEvolutionChain from '../../../../lib/hooks/useEvolutionChain';
 import { styled } from 'styled-components';
+import { EvolutionInfo, SpeciesInfoTypes } from '../../../../types';
+import Loading from '../../../Loading';
 
 interface EvolutionChainProps {
-  specyData: {
-    evolution_chain?: {
-      url: string;
-    };
-    evolves_from_species?: {
-      name: string;
-      url: string;
-    } | null;
-  };
+  species: SpeciesInfoTypes;
 }
 
-function EvolutionChain({ specyData }: EvolutionChainProps) {
-  const { data: evolutionInfo } = useGetEvolutionChain(
-    specyData?.evolution_chain?.url as string,
+function EvolutionChain({ species }: EvolutionChainProps) {
+  const { data: evolutionInfo, isLoading } = useGetEvolutionChain(
+    species?.evolution_chain?.url as string,
   );
 
   const { evoChain } = useEvolutionChain(
-    evolutionInfo,
-    specyData.evolves_from_species,
+    evolutionInfo as EvolutionInfo,
+    species?.evolves_from_species,
   );
+
+  if (isLoading) return <Loading />;
 
   return (
     <EvolutionChainContainer>
-      {evoChain?.map((evo, idx) => (
-        <Fragment key={idx}>
-          {!specyData.evolves_from_species && !idx && <Arrow>{'➡'}</Arrow>}
-          <div>
-            <EvolutionImg
-              src={findPokemonDBImage(evo?.name as string)}
-              alt="pokemon image"
-            />
-            <EvolutionName key={idx}>{evo?.name}</EvolutionName>
-          </div>
-          {idx < evoChain.length - 1 && <Arrow>{'➡'}</Arrow>}
-        </Fragment>
-      ))}
+      {evoChain.length ? (
+        <Suspense fallback={<Loading />}>
+          {evoChain?.map((evo, idx) => (
+            <Fragment key={idx}>
+              {!species?.evolves_from_species && !idx && (
+                <>
+                  <div>
+                    <EvolutionImg
+                      src={findPokemonDBImage(
+                        evolutionInfo?.species.name as string,
+                      )}
+                      alt="pokemon image"
+                    />
+                    <EvolutionName>{evolutionInfo?.species.name}</EvolutionName>
+                  </div>
+                  <Arrow>{'➡'}</Arrow>
+                </>
+              )}
+              <div>
+                <EvolutionImg
+                  src={findPokemonDBImage(evo?.name as string)}
+                  alt="pokemon image"
+                />
+                <EvolutionName key={idx}>{evo?.name}</EvolutionName>
+              </div>
+              {idx < evoChain.length - 1 && <Arrow>{'➡'}</Arrow>}
+            </Fragment>
+          ))}
+        </Suspense>
+      ) : (
+        <h2>진화 단계 없음</h2>
+      )}
     </EvolutionChainContainer>
   );
 }
 
 const EvolutionChainContainer = styled.div`
   display: flex;
+  height: 35%;
   flex-direction: row;
   justify-content: space-evenly;
   align-items: center;
@@ -72,4 +88,4 @@ const Arrow = styled.div`
   font-weight: 500;
 `;
 
-export default EvolutionChain;
+export default memo(EvolutionChain);
