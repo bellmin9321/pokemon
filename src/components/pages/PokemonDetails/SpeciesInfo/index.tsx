c3b6bf927a1f24442d8efb4fca42db37fb5063b5
JONGMIN KIM <bellmin9321@gmail.com>
2023-06-16 04:13:23 +0900

    ✨ Feat: pokemon 기타 정보 추가 및 UI 구현

--- src/components/pages/PokemonDetails/SpeciesInfo/index.tsx
+++ src/components/pages/PokemonDetails/SpeciesInfo/index.tsx
@@ -1,9 +1,14 @@
-import { Suspense } from 'react';
+import { Suspense, useCallback } from 'react';
 import { styled } from 'styled-components';
 import Loading from '../../../Loading';
-import { PokemonData } from '../../../../types';
+import {
+  PokemonData,
+  SpeciesInfoTypes,
+  flavor_text_entries,
+} from '../../../../types';
 import { useGetSpeciesInfo } from '../../../../lib/hooks/usePokemon';
 import EvolutionChain from '../EvolutionChain';
+import { capitalize } from '../../../../lib/utils';
 
 interface SpeciesInfoProps {
   pokemonData?: PokemonData;
@@ -14,28 +19,104 @@ function SpeciesInfo({ pokemonData }: SpeciesInfoProps) {
     pokemonData?.species.url as string,
   );
 
-  const koreanDescription = speciyInfo?.flavor_text_entries
-    ?.filter((text) => text.language.name === 'ko')
-    .reduce((a, b) => (a += `${b.flavor_text}\n`), '');
+  const koreanDescription = useCallback(
+    (descs: flavor_text_entries[]) =>
+      descs
+        ?.filter((text) => text.language.name === 'ko')
+        .reduce((a, b) => (a += `${b.flavor_text}\n`), ''),
+    [speciyInfo],
+  );
 
   return (
-    <PokemonDescriptionContainer>
-      <h2>포켓몬 설명</h2>
-      <Suspense fallback={<Loading />}>{koreanDescription}</Suspense>
+    <SpeciesInfoContainer>
+      <DescriptionBox>
+        <h2>포켓몬 설명</h2>
+        <Suspense fallback={<Loading />}>
+          <span>
+            {koreanDescription(
+              speciyInfo?.flavor_text_entries as flavor_text_entries[],
+            )}
+          </span>
+        </Suspense>
+      </DescriptionBox>
+      <FeaturesBox>
+        <div>
+          <h3>능력</h3>
+          <AbilityRow>
+            {pokemonData?.abilities.map((abilityObj, idx) => (
+              <span key={idx}>{capitalize(abilityObj.ability.name)}</span>
+            ))}
+          </AbilityRow>
+        </div>
+        <div>
+          <h3>잡을 확률</h3>
+          <div>{speciyInfo?.capture_rate}%</div>
+        </div>
+        <div>
+          <h3>행복도</h3>
+          <div>{speciyInfo?.base_happiness}%</div>
+        </div>
+        <div>
+          <h3>색상</h3>
+          <div>{capitalize(speciyInfo?.color?.name as string)}</div>
+        </div>
+      </FeaturesBox>
       <h2>진화 단계</h2>
       <Suspense fallback={<Loading />}>
-        <EvolutionChain specyData={speciyInfo || {}} />
+        <EvolutionChain species={speciyInfo as SpeciesInfoTypes} />
       </Suspense>
-    </PokemonDescriptionContainer>
+    </SpeciesInfoContainer>
   );
 }
 
-const PokemonDescriptionContainer = styled.div`
+const SpeciesInfoContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: 80%;
+  justify-content: center;
+  height: 100%;
   width: 50rem;
   padding: 30px;
+`;
+
+const DescriptionBox = styled.div`
+  > span {
+    font-size: 15px;
+  }
+`;
+
+const FeaturesBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 25%;
+  width: 85%;
+  align-items: center;
+  justify-content: space-between;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    > div {
+      font-size: 20px;
+      font-weight: 500;
+      color: #3e3e3e;
+    }
+  }
+`;
+
+const AbilityRow = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  > span {
+    padding: 5px 10px;
+    border: 1px solid lightgray;
+    border-radius: 15px;
+    font-weight: 500;
+    margin-right: 10px;
+    font-size: 13px;
+  }
 `;
 
 export default SpeciesInfo;
