da9c4c857c883009f8b65afe52d02220bc5db3bf
JONGMIN KIM <bellmin9321@gmail.com>
2023-06-15 21:19:43 +0900

    ✅ Add: constants 추가

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+// String
+export const INPUT_PLACEHOLDER = `1~1010번까지 검색 가능`;
+export const UNKNOWN = '알 수 없음';
+
+// Number
+export const DEFAULT_LIMIT = 20;
+export const DEFAULT_LIST_QUANTITY = 20;
+export const POKEMON_LIST_MAX_LENGTH = 51;
