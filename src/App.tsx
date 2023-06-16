bb254b910e9c1b5784d1e337326aeccb6a685199
JONGMIN KIM <bellmin9321@gmail.com>
2023-06-16 12:18:50 +0900

    🚚 Rename: 파일명에 s 추가하여 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React, { Suspense } from 'react';
 import './App.css';
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from './lib/api/queryClient';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout';
 import Home from './components/pages/Home';
 import { RecoilRoot } from 'recoil';
 import Loading from './components/Loading';
-import PokemonDetails from './components/pages/PokemonDetail';
+import PokemonDetails from './components/pages/PokemonDetails';
 import NotFound from './components/pages/NotFound';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
